529c2ace8591f1a18b058cddedc4d772ab023e9c
Helena Barbosa Costa <helenacosta-ieg@instituto.ad>
2024-04-26 08:40:22 -0300

    first commit

---
+++ src/main/java/com/example/nat_glam/NatGlamApplication.java
@@ -0,0 +1,13 @@
+package com.example.nat_glam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatGlamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NatGlamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nat_glam/NatGlamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nat_glam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NatGlamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
