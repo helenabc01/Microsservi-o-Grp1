a91eba9bfec0869f0a5d9c760d45ab5586fa49f2
Helena Barbosa Costa <helenacosta-ieg@instituto.ad>
2024-04-29 07:52:56 -0300

    validação de cpf

--- src/main/java/com/example/nat_glam/NatGlamApplication.java
+++ src/main/java/com/example/nat_glam/NatGlamApplication.java
@@ -4,13 +4,34 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.kafka.annotation.EnableKafka;
+import user.User;
+
+import java.util.Scanner;
 
 @EnableKafka
 @SpringBootApplication
 public class NatGlamApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(NatGlamApplication.class, args);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o CPF:");
+        String cpf = scanner.nextLine();
+
+        if (User.isValidCPF(cpf)) {
+            System.out.println("CPF válido!");
+        } else {
+            System.out.println("CPF inválido!");
+
+            if (cpf.length() != 11) {
+                System.out.println("O CPF deve ter 11 dígitos.");
+            } else if (cpf.matches("(\\d)\\1{10}")) {
+                System.out.println("Todos os dígitos do CPF são iguais.");
+            } else {
+                System.out.println("O CPF falhou no teste de verificação.");
+            }
+        }
+        scanner.close();
     }
 
 }
--- src/main/java/user/User.java
+++ src/main/java/user/User.java
@@ -1,6 +1,7 @@
 package user;
 
 import java.time.LocalDate;
+import java.util.InputMismatchException;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -122,6 +123,36 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+
+    public static boolean isValidCPF(String cpf){
+        cpf = cpf.replaceAll("[^0-9]", "");
+        if (cpf.length() != 11) return false;
+        if (cpf.matches("(\\d)\\1{10}")) return false;
+
+        try {
+            int sum = 0;
+            for (int i = 0; i < 9; i++) {
+                sum += Integer.parseInt(cpf.substring(i, i + 1)) * (10 - i);
+            }
+
+            int digit1 = 11 - (sum % 11);
+            if (digit1 > 9) digit1 = 0;
+
+            sum = 0;
+            for (int i = 0; i < 10; i++) {
+                sum += Integer.parseInt(cpf.substring(i, i + 1)) * (11 - i);
+            }
+            int digit2 = 11 - (sum % 11);
+            if (digit2 > 9) digit2 = 0;
+
+            return Integer.parseInt(cpf.substring(9, 10)) == digit1 && Integer.parseInt(cpf.substring(10, 11)) == digit2;
+
+        } catch (InputMismatchException e){
+            return false;
+        }
+
     }
 
     @Override
