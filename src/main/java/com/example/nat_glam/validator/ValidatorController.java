e01ed1a426e7a65d4e493552879e109814b41d6d
Helena Barbosa Costa <helenacosta-ieg@instituto.ad>
2024-04-29 09:09:00 -0300

    validator

--- src/main/java/com/example/nat_glam/validator/ValidatorController.java
+++ src/main/java/com/example/nat_glam/validator/ValidatorController.java
@@ -1,4 +1,17 @@
 package com.example.nat_glam.validator;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("validator")
 public class ValidatorController {
+    public ValidatorController() {
+    }
+
+    @GetMapping("/validateuser/{cpf}")
+    public Boolean Validate(@PathVariable("cpf")Long cpf){
+
+    }
+
 }
--- src/main/java/com/example/nat_glam/validator/ValidatorService.java
+++ src/main/java/com/example/nat_glam/validator/ValidatorService.java
@@ -1,4 +1,37 @@
 package com.example.nat_glam.validator;
 
+import org.springframework.stereotype.Service;
+
+import java.util.InputMismatchException;
+@Service
 public class ValidatorService {
+
+    public static boolean isValidCPF(String cpf){
+        cpf = cpf.replaceAll("[^0-9]", "");
+        if (cpf.length() != 11) return false;
+        if (cpf.matches("(\\d)\\1{10}")) return false;
+
+        try {
+            int sum = 0;
+            for (int i = 0; i < 9; i++) {
+                sum += Integer.parseInt(cpf.substring(i, i + 1)) * (10 - i);
+            }
+
+            int digit1 = 11 - (sum % 11);
+            if (digit1 > 9) digit1 = 0;
+
+            sum = 0;
+            for (int i = 0; i < 10; i++) {
+                sum += Integer.parseInt(cpf.substring(i, i + 1)) * (11 - i);
+            }
+            int digit2 = 11 - (sum % 11);
+            if (digit2 > 9) digit2 = 0;
+
+            return Integer.parseInt(cpf.substring(9, 10)) == digit1 && Integer.parseInt(cpf.substring(10, 11)) == digit2;
+
+        } catch (InputMismatchException e){
+            return false;
+        }
+
+    }
 }
